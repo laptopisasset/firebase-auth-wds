38aa5f8d742b00bdce8a72055317070a610e5b93
Soundhar <laptopisasset@gmail.com>
2021-06-12 18:21:16 +0530

    Login Jsx Added.

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,33 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Signup from "./Signup";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
 
 import { AuthProvider } from "../context/AuthContext";
 
 function App() {
   return (
-    <AuthProvider>
-      <Container
-        className="d-flex align-items-center justify-content-center"
-        style={{
-          minHeight: "100vh",
-        }}
-      >
-        <div className="w-100" style={{ maxWidth: "400px" }}>
-          <Signup />
-        </div>
-      </Container>
-    </AuthProvider>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{
+        minHeight: "100vh",
+      }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <Switch>
+            <AuthProvider>
+              <Route exact path="/" component={Dashboard} />
+              <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
+            </AuthProvider>
+          </Switch>
+        </Router>
+      </div>
+    </Container>
   );
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -3,10 +3,9 @@ import { Form, Button, Card, Alert } from "react-bootstrap";
 import { useAuth } from "../context/AuthContext";
 import { Link } from "react-router-dom";
 
-const Signup = () => {
+const Login = () => {
   const emailRef = useRef();
   const passwordRef = useRef();
-  const passwordConfirmRef = useRef();
   const { signUp } = useAuth();
 
   const [error, setError] = useState("");
@@ -15,16 +14,12 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-      return setError("Passwords do not match");
-    }
-
     try {
       setError("");
       setLoading(true);
       await signUp(emailRef.current.value, passwordRef.current.value);
     } catch {
-      setError("Failed to create an account");
+      setError("Failed to Log In");
       setLoading(false);
     }
   };
@@ -33,7 +28,7 @@ const Signup = () => {
     <>
       <Card>
         <Card.Body>
-          <h2 className="text-center mb-4">Sign Up</h2>
+          <h2 className="text-center mb-4">Log In</h2>
           {error && <Alert variant="danger">{error}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
@@ -44,21 +39,17 @@ const Signup = () => {
               <Form.Label>Password</Form.Label>
               <Form.Control type="password" ref={passwordRef} required />
             </Form.Group>
-            <Form.Group id="password-confirm">
-              <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control type="password" ref={passwordConfirmRef} required />
-            </Form.Group>
             <Button disabled={lodaing} type="submit" className="mt-2 w-100">
-              Sign Up
+              Log In
             </Button>
           </Form>
         </Card.Body>
       </Card>
       <div className="w-100 text-center mt-2">
-        Already have an account? <Link to="/login">Log in</Link>
+        Need an account? <Link to="/signup">Sign Up</Link>
       </div>
     </>
   );
 };
 
-export default Signup;
+export default Login;
